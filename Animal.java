abab4f2631d767c4d6ed8fd96fe234365174757a
Sander Weiss <weisssander@gmail.com>
2023-04-12 20:51:48 -0400

    initial boiler plate

---
+++ Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+
+    public static String iAmDog() {
+        return "I am a dog";
+    }
+
+    public static void doStuff() {
+
+        int a = 5;
+
+        if (a == 0) {
+
+        } else if (a == 1) {
+
+        } else {
+
+        }
+    }
+}
---
+++ LearnJava.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class LearnJava {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+    }
+}
